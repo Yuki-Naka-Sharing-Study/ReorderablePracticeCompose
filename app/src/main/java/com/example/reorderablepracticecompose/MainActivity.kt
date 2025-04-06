8981e13181b8e73cf1ec600dcf1fe8aedf5b6378
仲優樹 <yu-ki216m@outlook.jp>
2025-04-06 14:56:55 +0900

    Created ReorderableScreen.

--- app/src/main/java/com/example/reorderablepracticecompose/MainActivity.kt
+++ app/src/main/java/com/example/reorderablepracticecompose/MainActivity.kt
@@ -4,14 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalView
 import com.example.reorderablepracticecompose.ui.theme.ReorderablePracticeComposeTheme
+import sh.calvin.reorderable.rememberReorderableLazyListState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.unit.dp
+import androidx.core.view.HapticFeedbackConstantsCompat
+import androidx.core.view.ViewCompat
+import sh.calvin.reorderable.ReorderableItem
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +39,64 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ReorderablePracticeComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ReorderableScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun ReorderableScreen() {
+    val view = LocalView.current
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ReorderablePracticeComposeTheme {
-        Greeting("Android")
+    var list by remember { mutableStateOf(List(100) { "Item $it" }) }
+    val lazyListState = rememberLazyListState()
+    val reorderableLazyListState = rememberReorderableLazyListState(lazyListState) { from, to ->
+        list = list.toMutableList().apply {
+            add(to.index, removeAt(from.index))
+        }
+
+        ViewCompat.performHapticFeedback(
+            view,
+            HapticFeedbackConstantsCompat.SEGMENT_FREQUENT_TICK
+        )
+    }
+
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        state = lazyListState,
+        contentPadding = PaddingValues(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        items(list, key = { it }) {
+            ReorderableItem(reorderableLazyListState, key = it) { isDragging ->
+                val elevation by animateDpAsState(if (isDragging) 4.dp else 0.dp)
+
+                Surface(shadowElevation = elevation) {
+                    Row {
+                        Text(it, Modifier.padding(horizontal = 8.dp))
+                        IconButton(
+                            modifier = Modifier.draggableHandle(
+                                onDragStarted = {
+                                    ViewCompat.performHapticFeedback(
+                                        view,
+                                        HapticFeedbackConstantsCompat.GESTURE_START
+                                    )
+                                },
+                                onDragStopped = {
+                                    ViewCompat.performHapticFeedback(
+                                        view,
+                                        HapticFeedbackConstantsCompat.GESTURE_END
+                                    )
+                                },
+                            ),
+                            onClick = {},
+                        ) {
+                            Icon(Icons.Rounded.Menu, contentDescription = "Reorder")
+                        }
+                    }
+                }
+            }
+        }
     }
 }
